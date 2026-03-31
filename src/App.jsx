f36ae0229fa62b38d24b6e7deeed4dd4da4053be
Jon Gudenzi <jongudenzi@gmail.com>
2026-03-30 20:49:44 -0400

    error state setup

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ export default function App() {
   const [cityInput, setCityInput] = useState("");
   const [loading, setLoading] = useState(false);
   const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
 
   function handleSubmit() {
     setLoading(true);
+    setError(null);
 
     fetch("https://jsonplaceholder.typicode.com/posts/1")
       .then(response => response.json())
       .then(function (data) {
         setWeatherData(data);
         console.log(data);
-      })
-      .then(() => setLoading(false));
+        setLoading(false);
+      });
   }
 
   return (
@@ -31,6 +33,7 @@ export default function App() {
         disabled={loading}>Submit</button>
 
       {loading && <p>loading...</p>}
+      {error && <p>{error}</p>}
       {weatherData && (
         <>
           <p>{weatherData.title}</p>
